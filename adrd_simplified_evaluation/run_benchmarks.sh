651f1b1ffa8e40c38df210b287c8960bb7235b41
Matteo Bellitti <bellitti@bu.edu>
2025-08-06 10:07:57 -0400

    moved options to config file from sh

--- adrd_simplified_evaluation/run_benchmarks.sh
+++ adrd_simplified_evaluation/run_benchmarks.sh
@@ -19,13 +19,14 @@ module load miniconda
 module load cuda
 
 export HF_HOME=/projectnb/vkolagrp/skowshik/.cache
+# export HF_HOME=/projectnb/vkolagrp/bellitti/hf_cache
 
 export VLLM_SKIP_P2P_CHECK=1
 
 # set to 1 to execute gpu operations synchronously
 # export CUDA_LAUNCH_BLOCKING=1 
 
-cd adrd_simplified_evaluation
+cd /projectnb/vkolagrp/bellitti/adrd-foundation-model/adrd_simplified_evaluation 
 
 # conda activate /projectnb/vkolagrp/projects/adrd_foundation_model/envs/fmadrd
 conda activate /projectnb/vkolagrp/skowshik/conda_envs/vllm_env
@@ -39,21 +40,19 @@ while true; do
     if [ "$count" -lt 4 ]; then
         echo "GPU is idle with $count Python processes. Starting next script..."
         pids=$(nvidia-smi | grep python | awk '{print $5}')
+
         # for pid in $pids; do
         #     echo "Killing process with PID $pid"
         #     kill -9 "$pid"
         # done
 
-        # Now start your script
-        # echo "Starting benchmark evaluation script..."
+        python src/main.py config_file=config.yml
 
-        # python src/main.py config_file=config.yml
-
-        python src/main.py config_file=config.yml  \
-        model_name='["/projectnb/vkolagrp/skowshik/foundation_adrd/adrd-foundation-model/open-r1/ckpt_using_subset/qwen25_3B_drgrpo_gp8_train_filtered_sub", "/projectnb/vkolagrp/skowshik/foundation_adrd/adrd-foundation-model/open-r1/ckpt_using_subset/qwen25_3B_drgrpo_gp8_train_filtered_sub_no_KL"]' \
-        benchmarks='["benchmarks/nacc_test/test_np", "benchmarks/nacc_test/test_mci", "benchmarks/nacc_test/test_cog"]' \
-        template_style="grpo" \
-        system_prompt="Please reason step by step, and put your final answer within \\boxed{}."
+        # python src/main.py config_file=config.yml  \
+        # model_name='["/projectnb/vkolagrp/skowshik/foundation_adrd/adrd-foundation-model/open-r1/ckpt_using_subset/qwen25_3B_drgrpo_gp8_train_filtered_sub", "/projectnb/vkolagrp/skowshik/foundation_adrd/adrd-foundation-model/open-r1/ckpt_using_subset/qwen25_3B_drgrpo_gp8_train_filtered_sub_no_KL"]' \
+        # benchmarks='["benchmarks/nacc_test/test_np", "benchmarks/nacc_test/test_mci", "benchmarks/nacc_test/test_cog"]' \
+        # template_style="grpo" \
+        # system_prompt="Please reason step by step, and put your final answer within \\boxed{}."
 
         # python src/main.py config_file=config.yml  \
         # model_name='["Qwen/Qwen2.5-3B-Instruct", "/projectnb/vkolagrp/skowshik/foundation_adrd/adrd-foundation-model/open-r1/ckpt_using_subset/qwen25_3B_drgrpo_gp8_all_train_sub_old_prompt"]' \
